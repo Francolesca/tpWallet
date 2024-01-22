c60e25b68a6b90480d9f630f306ed623e66a9f6b
Francolesca <88465161+Francolesca@users.noreply.github.com>
2024-01-22 19:12:46 -0300

    vista login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,38 @@
+<template>
+  <form @submit.prevent="login">
+    <div>
+      <label for="username">Usuario:</label>
+      <input type="text" id="username" v-model="username" required />
+    </div>
+    <div>
+      <label for="password">Contraseña:</label>
+      <input type="password" id="password" v-model="password" required />
+    </div>
+    <div>
+      <button type="submit">Iniciar sesión</button>
+    </div>
+    <div>
+      <span>¿No tienes cuenta? <a href="#">Registrate</a></span><br>
+      <span>¿Olvidaste la contraseña? <a href="#">Recupera tu contraseña</a></span>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    login() {
+      console.log('Iniciar sesión con:', this.username, this.password);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
