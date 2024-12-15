b3f0fc627e6343afa7dd234da3e9d28260138042
Francolesca <franco.sistemas1999@gmail.com>
2024-12-15 19:10:04 -0300

    Agregado componente Nav

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
 <template id="app">
-  <div class="nav-container">
-    <nav>
-      <router-link class="nav-link" to="/home">Home</router-link> |
-      <router-link class="nav-link" to="/transaction">Trade</router-link> |
-      <router-link class="nav-link" to="/history">History</router-link> |
-      <router-link class="nav-link" to="/current">Current Analysis</router-link> |
-      <router-link class="nav-link" to="/investment">Investment Analysis</router-link>
-    
-    </nav>
+  <div id="app">
+    <NavBar />
+    <router-view />
   </div>
-  <router-view/>
 </template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+
 
 <style scoped>
 #app {
-    background-color: #121212; 
-    color: #ffffff; 
+    background-color: #ffffff; 
+    color: #000000; 
     min-height: 100vh; 
     font-family: Arial, sans-serif;
 }
-.nav-container {
-    padding: 20px;
-    background-color: #ffffff;
-    border-bottom: 2px solid #007bff;
-}
-
-.navbar {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-}
-
-.nav-link {
-    color: #007bff;
-    text-decoration: none;
-    padding: 10px 15px;
-    border-radius: 5px;
-    transition: background-color 0.3s, color 0.3s;
-}
-
-.nav-link:hover {
-    background-color: #007bff;
-    color: white;
-}
-
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<!-- NavBar.vue -->
+<template>
+    <div class="nav-container">
+      <nav class="navbar">
+        <router-link class="nav-link" to="/home">Home</router-link>
+        <router-link class="nav-link" to="/transaction">Trade</router-link>
+        <router-link class="nav-link" to="/history">History</router-link>
+        <router-link class="nav-link" to="/current">Current Analysis</router-link>
+        <router-link class="nav-link" to="/investment">Investment Analysis</router-link>
+      </nav>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavBar',
+  };
+  </script>
+  
+  <style scoped>
+  .nav-container {
+      padding: 20px;
+      background-color: #ffffff;
+      border-bottom: 2px solid #007bff;
+  }
+  
+  .navbar {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+  }
+  
+  .nav-link {
+      color: #007bff;
+      text-decoration: none;
+      padding: 10px 15px;
+      border-radius: 5px;
+      transition: background-color 0.3s, color 0.3s;
+  }
+  
+  .nav-link:hover {
+      background-color: #007bff;
+      color: white;
+  }
+  </style>
+  
